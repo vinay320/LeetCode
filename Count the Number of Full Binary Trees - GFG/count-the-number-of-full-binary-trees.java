e2f28872be50474f5aa17770fe7bb1725430e56d
vinay320 <85616775+vinay320@users.noreply.github.com>
2022-11-29 07:02:06 +0530

    Added solution - LeetHub

---
+++ Count the Number of Full Binary Trees - GFG/count-the-number-of-full-binary-trees.java
@@ -0,0 +1,93 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class GFG {
+	public static void main(String[] args) throws IOException
+	{
+	        BufferedReader br =
+            new BufferedReader(new InputStreamReader(System.in));
+        int t =
+            Integer.parseInt(br.readLine().trim()); // Inputting the testcases
+        while(t-->0)
+        {
+            int n = Integer.parseInt(br.readLine().trim());
+            int a[] = new int[(int)(n)];
+            String inputLine[] = br.readLine().trim().split(" ");
+            for (int i = 0; i < n; i++) {
+                a[i] = Integer.parseInt(inputLine[i]);
+            }
+            
+            Solution obj = new Solution();
+            System.out.println(obj.numoffbt(a, n));
+            
+        }
+	}
+}
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution {
+    
+    public static int numoffbt(int arr[], int n)
+    {
+         // Your code goes here
+         int maxvalue = Integer.MIN_VALUE;
+        int minvalue = Integer.MAX_VALUE;
+        for (int i = 0; i < n; i++)
+        {
+            maxvalue = Math.max(maxvalue, arr[i]);
+            minvalue = Math.min(minvalue, arr[i]);
+        }
+     
+        int mark[] = new int[maxvalue + 2];
+        int value[] = new int[maxvalue + 2];
+        Arrays.fill(mark, 0);
+        Arrays.fill(value, 0);
+     
+
+        for (int i = 0; i < n; i++)
+        {
+            mark[arr[i]] = 1;
+            value[arr[i]] = 1;
+        }
+     
+
+        int ans = 0;
+        for (int i = minvalue; i <= maxvalue; i++)
+        {
+             
+ 
+            if (mark[i] != 0)
+            {
+
+                for (int j = i + i;
+                    j <= maxvalue && j/i <= i; j += i)
+                {
+  
+                    if (mark[j] == 0)
+                        continue;
+     
+   
+                    value[j] = value[j] + (value[i]
+                                          * value[j/i]);
+     
+
+                    if (i != j / i)
+                        value[j] = value[j] + (value[i]
+                                         * value[j/i]);
+                }
+            }
+     
+            ans += value[i] %1000000007;
+        }
+     
+        return ans % 1000000007;
+    }
+}
