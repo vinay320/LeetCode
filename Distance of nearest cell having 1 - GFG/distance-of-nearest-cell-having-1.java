9eb85f921f3c3636b23db71f50b6b11ac40484d2
vinay320 <85616775+vinay320@users.noreply.github.com>
2022-12-06 05:55:49 +0530

    Added solution - LeetHub

---
+++ Distance of nearest cell having 1 - GFG/distance-of-nearest-cell-having-1.java
@@ -0,0 +1,77 @@
+//{ Driver Code Starts
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class GFG
+{
+    public static void main(String[] args) throws IOException
+    {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine().trim());
+        while(T-->0)
+        {
+            String[] s = br.readLine().trim().split(" ");
+            int n = Integer.parseInt(s[0]);
+            int m = Integer.parseInt(s[1]);
+            int[][] grid = new int[n][m];
+            for(int i = 0; i < n; i++){
+                String[] S = br.readLine().trim().split(" ");
+                for(int j = 0; j < m; j++){
+                    grid[i][j] = Integer.parseInt(S[j]);
+                }
+            }
+            Solution obj = new Solution();
+            int[][] ans = obj.nearest(grid);
+            for(int i = 0; i < ans.length; i++){
+                for(int j = 0; j < ans[i].length; j++){
+                    System.out.print(ans[i][j] + " ");
+                }
+                System.out.println();
+            }
+        }
+    }
+}
+// } Driver Code Ends
+
+
+class Solution
+{
+    //Function to find distance of nearest 1 in the grid for each cell.
+   private boolean isSafe(int[][] grid, int row, int col) {
+		if (row < 0 || row >= grid.length || col < 0 || col >= grid[0].length || grid[row][col] != -1) {
+			return false;
+		}
+		return true;
+	}
+
+	public int[][] nearest(int[][] grid) {
+		Queue<int[]> queue = new LinkedList<>();
+		for (int row = 0; row < grid.length; row++) {
+			for (int col = 0; col < grid[0].length; col++) {
+				if (grid[row][col] == 1) {
+					grid[row][col] = 0;
+					queue.offer(new int[] { row, col });
+				} else {
+					grid[row][col] = -1;
+				}
+			}
+		}
+		int[] dirX = { -1, 1, 0, 0 };
+		int[] dirY = { 0, 0, -1, 1 };
+		while (!queue.isEmpty()) {
+			int size = queue.size();
+			for (int i = 0; i < size; i++) {
+				int[] rowCol = queue.poll();
+				for (int j = 0; j < 4; j++) {
+					int row = rowCol[0] + dirX[j];
+					int col = rowCol[1] + dirY[j];
+					if (isSafe(grid, row, col)) {
+						grid[row][col] = 1 + grid[rowCol[0]][rowCol[1]];
+						queue.offer(new int[] { row, col });
+					}
+				}
+			}
+		}
+		return grid;
+	}
+}
