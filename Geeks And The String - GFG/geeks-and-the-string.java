46e935547422473907996a09407efe88398f3f77
Vinay Tomar <85616775+vinay320@users.noreply.github.com>
2023-01-23 05:53:49 +0530

    Added solution - LeetHub

---
+++ Geeks And The String - GFG/geeks-and-the-string.java
@@ -0,0 +1,56 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t;
+        t = Integer.parseInt(br.readLine());
+        while(t-- > 0){
+            
+            String s; 
+            s = br.readLine();
+            
+            Solution obj = new Solution();
+            String res = obj.removePair(s);
+            
+            System.out.println(res);
+            
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+class Solution {
+    public static String removePair(String s) {
+        // code here
+        String x="";
+        List<Character> l=new ArrayList<>();
+        for(int i=0;i<s.length();i++)
+        {
+                if(l.size()>0 && l.get(l.size()-1)==s.charAt(i))
+                {
+                    l.remove(l.size()-1);
+                }
+                else
+                {
+                    l.add(s.charAt(i));
+                }
+                
+            
+        }
+        for(int i=0;i<l.size();i++)
+        {
+            x+=l.get(i);
+        }
+        if(x.length()==0)
+        {
+            return "-1";
+        }
+        return x;
+    }
+}
+        
