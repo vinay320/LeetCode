6fc719e94a2fa88d0ad779c81e8913c095c53f54
Vinay Tomar <85616775+vinay320@users.noreply.github.com>
2023-03-15 19:17:31 +0530

    Time: 41 ms (57.19%), Space: 58.3 MB (78.83%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.java
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.java
@@ -1,30 +1,40 @@
 class Solution {
-    public int longestConsecutive(int[] nums) {
-        if(nums.length==0)
-        {
-            return 0;
-        }
-        Arrays.sort(nums);
-        int max=1;
-        int ans=1;
+    public int longestConsecutive(int[] arr) {
+        Map<Integer,Boolean> m=new HashMap<>();
         
-        for(int i=1;i<nums.length;i++)
+        // agar ek kam wala exist karta h to curr ko false krdo
+        for(int i=0;i<arr.length;i++)
         {
-            if(nums[i]==nums[i-1])
+            if(m.containsKey(arr[i]-1))
             {
-                continue;
-            }
-            if(nums[i]-nums[i-1]<=1)
-            {
-                max++;
+                m.put(arr[i],false);
             }
             else
             {
-                max=1;
+                m.put(arr[i],true);
             }
-            ans=Math.max(max,ans);
+            // agr ek jada wala exist karta h to jada wale ko false krdo
+            if(m.containsKey(arr[i]+1))
+            {
+                m.put(arr[i]+1,false);
+            }
         }
         
+        int ans=0;
+        for(int key:m.keySet())
+        {
+            if(m.get(key)==true)
+            {
+                int c=0;
+                while(m.containsKey(key))
+                {
+                    c++;
+                    key++;
+                }
+                 ans=Math.max(ans,c);
+            }
+           
+        }
         return ans;
     }
 }
