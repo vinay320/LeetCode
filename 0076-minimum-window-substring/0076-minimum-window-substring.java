829159c1e1c52cddca7c4af29e7f81b8c0984598
Vinay Tomar <85616775+vinay320@users.noreply.github.com>
2023-04-02 13:34:07 +0530

    Time: 216 ms (13.06%), Space: 44.4 MB (8.37%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.java
+++ 0076-minimum-window-substring/0076-minimum-window-substring.java
@@ -1,38 +1,68 @@
 class Solution {
-   
+    public String minWindow(String s, String p) {
+        String ans="";
+        HashMap<Character,Integer> m=new HashMap<>();
+        for(int i=0;i<p.length();i++)
+        {
+            m.put(p.charAt(i),m.getOrDefault(p.charAt(i),0)+1);
+        }
         
-    boolean checkforValidSubstring(int[] a1, int[] a2, int n) {
-        for (int i = 0; i < n; i++) {
-            if (a1[i] < a2[i])
-                return false;
-        }
-        return true;
-    }
-
-    public String minWindow(String s, String t) {
-        int m = s.length();
-        int n = t.length();
-        if (n > m)
-            return "";
-        int[] arr = new int[60];
-        for (int i = 0; i < n; i++)
-            arr[t.charAt(i) - 'A']++;
-        int st = 0, end = 0;
-        int min = Integer.MAX_VALUE;
-        String res = "";
-        int[] arr2 = new int[60];
-        while (end < m) {
-            arr2[s.charAt(end) - 'A']++;
-            while (st<=end && checkforValidSubstring(arr2, arr, 60)) {
-                if(min>end-st+1){
-                    min = end-st+1;
-                    res = s.substring(st,end+1);
+        int mct=0;
+        int dmct=p.length();
+        
+        HashMap<Character,Integer> m1=new HashMap<>();
+        int i=-1;
+        int j=-1;
+        while(true)
+        {
+            boolean f1=false;
+            boolean f2=false;
+            while(i<s.length()-1 && mct<dmct)
+            {
+                i++;
+                m1.put(s.charAt(i),m1.getOrDefault(s.charAt(i),0)+1);
+                
+                if(m1.getOrDefault(s.charAt(i),0)<=m.getOrDefault(s.charAt(i),0))
+                {
+                    mct++;
                 }
-                arr2[s.charAt(st)-'A']--;
-                st++;
+                f1=true;
             }
-            end++;
+            
+            while(j<i && mct==dmct)
+            {
+                String pans=s.substring(j+1,i+1); 
+                if(ans.length()==0 || pans.length()<ans.length())
+                {
+                    ans=pans;
+                }
+                
+                j++;
+                char ch=s.charAt(j);
+                if(m1.get(ch)==1)
+                {
+                    m1.remove(ch);
+                }
+                else
+                {
+                    m1.put(ch,m1.get(ch)-1);
+                }
+                
+                if(m1.getOrDefault(ch,0)<m.getOrDefault(ch,0))
+                {
+                    mct--;
+                }
+                
+                f2=true;
+                
+            }
+            
+            if(f1==false && f2==false)
+            {
+                break;
+            }
         }
-        return res;
+        
+        return ans;
     }
 }
