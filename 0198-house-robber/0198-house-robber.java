47f34bf9f51806bfc18ceed4d70fbb9695e0e2a1
Vinay Tomar <85616775+vinay320@users.noreply.github.com>
2023-04-06 09:31:12 +0530

    Time: 0 ms (100.00%), Space: 39.7 MB (82.08%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -1,9 +1,7 @@
 class Solution {
     public int rob(int[] nums) {
-        
-        int[] dp=new int[nums.length];
-        Arrays.fill(dp,-1);
-        return rob(nums,0,dp);
+    
+        return robB(nums);
     }
     
     public static int rob(int[] arr,int i,int[] dp)
@@ -19,5 +17,25 @@ class Solution {
         int r=arr[i]+rob(arr,i+2,dp);
         int dr=rob(arr,i+1,dp);
         return dp[i]=Math.max(r,dr);
+    }
+    
+    public static int robB(int[] arr)
+    {
+       if(arr.length==1)
+       {
+           return arr[0];
+       }
+         int[] dp=new int[arr.length];
+        dp[0]=arr[0];
+        dp[1]=Math.max(arr[0],arr[1]);
+        
+        for(int i=2;i<arr.length;i++)
+        {
+            int x=arr[i]+dp[i-2];
+            int  y=dp[i-1];
+            
+            dp[i]=Math.max(x,y);
+        }
+        return dp[arr.length-1];
     }
 }
