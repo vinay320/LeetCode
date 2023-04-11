b9494b2c1079f4a60a114be73dc1c770f81c0ad4
Vinay Tomar <85616775+vinay320@users.noreply.github.com>
2023-04-11 15:00:29 +0530

    Time: 120 ms (32.29%), Space: 50.8 MB (33.21%) - LeetHub

---
+++ 0493-reverse-pairs/0493-reverse-pairs.java
@@ -0,0 +1,57 @@
+class Solution {
+    public int reversePairs(int[] nums) {
+        
+        return mst(nums,0,nums.length-1);
+        
+    }
+    
+    static int mst(int[] nums,int low,int high)
+    {
+        if(low>=high)
+        {
+            return 0;
+        }
+        int mid=(low+high)/2;
+        int inv=mst(nums,low,mid);
+        inv+=mst(nums,mid+1,high);
+        inv+=merge(nums,low,mid,high);
+        return inv;
+    }
+    
+    static int merge(int[] nums, int low, int mid, int high)
+    {
+        int cnt = 0;
+        int j = mid + 1; 
+        for(int i = low;i<=mid;i++) {
+            while(j<=high && nums[i] > (2 * (long) nums[j])) {
+                j++;
+            }
+            cnt += (j - (mid+1));
+        }
+        
+        ArrayList<Integer> temp = new ArrayList<>(); 
+        int left = low, right = mid+1; 
+        while(left <= mid && right<=high) {
+            if(nums[left]<=nums[right]) {
+                temp.add(nums[left++]); 
+            }
+            else {
+                temp.add(nums[right++]); 
+            }
+        }
+        
+        while(left<=mid) {
+            temp.add(nums[left++]); 
+        }
+        while(right<=high) {
+            temp.add(nums[right++]); 
+        }
+        
+        for(int i = low; i<=high;i++) {
+            nums[i] = temp.get(i - low); 
+        }
+        return cnt;
+                  
+        
+    }
+}
