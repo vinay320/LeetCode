980ca8cbeab368686d4b55415f99c2405e1fb59b
Vinay Tomar <85616775+vinay320@users.noreply.github.com>
2023-04-12 17:24:09 +0530

    Time: 43 ms (59.74%), Space: 49.6 MB (77.74%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,35 +1,46 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-        Arrays.sort(nums);
-        Set<List<Integer>> ans=new HashSet<>();
+       Arrays.sort(nums);
+         List<List<Integer>> list=new ArrayList<>();
+        
         for(int i=0;i<nums.length;i++)
         {
-            int j=i+1;
-            int k=nums.length-1;
-            while(j<k)
+            if(i==0 || (i>0 && nums[i]!=nums[i-1]) )
             {
-                if(-nums[i]==(nums[j]+nums[k]))
+                int low=i+1;
+                int high=nums.length-1;
+                int sum=0-nums[i];
+                while(low<high)
                 {
-                    List<Integer> list=new ArrayList<>();
-                    list.add(nums[i]);
-                    list.add(nums[j]);
-                    list.add(nums[k]);
-                    ans.add(new ArrayList<>(list));
-                    j++;
-                }
-                else if(-nums[i]>(nums[j]+nums[k]))
-                {
-                 j++;   
-                }
-                else
-                {
-                    k--;
+                    if(nums[low]+nums[high]==sum)
+                    {
+                        list.add(Arrays.asList(nums[i],nums[low],nums[high]));
+                        while(low<high && nums[low]==nums[low+1])
+                        {
+                            low++;
+                        }
+                        while(low<high && nums[high]==nums[high-1])
+                        {
+                            high--;
+                        }
+                        low++;
+                        high--;
+                    }
+                    else if(nums[low]+nums[high]>sum)
+                    {
+                        high--;
+                    }
+                    else
+                    {
+                        low++;
+                    }
+                        
                 }
             }
+            
+            
         }
-        
-        
-        return new ArrayList<>(ans);
+        return list;
         
     }
 }
