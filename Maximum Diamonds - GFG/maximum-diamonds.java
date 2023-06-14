1886b3d7b56f2ccc200e6ea5a0447281a8b3b046
Vinay Tomar <85616775+vinay320@users.noreply.github.com>
2023-06-14 07:29:08 +0530

    Added solution - LeetHub

---
+++ Maximum Diamonds - GFG/maximum-diamonds.java
@@ -0,0 +1,51 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String args[]) throws IOException {
+        BufferedReader read =
+            new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        while (t-- > 0) {
+            String S[] = read.readLine().split(" ");
+            
+            int N = Integer.parseInt(S[0]);
+            int K = Integer.parseInt(S[1]);
+            
+            int[] A = new int[N];
+            
+            String S1[] = read.readLine().split(" ");
+            for(int i=0; i<N; i++)
+                A[i] = Integer.parseInt(S1[i]);
+
+            Solution ob = new Solution();
+            System.out.println(ob.maxDiamonds(A,N,K));
+        }
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution {
+    static long maxDiamonds(int[] A, int N, int K) {
+        // code here4
+        PriorityQueue<Integer> p = new PriorityQueue<Integer>(Collections.reverseOrder());
+        for(int i=0;i<N;i++)
+        {
+            p.offer(A[i]);
+        }
+        long sum=0;
+        while(K-->0)
+        {
+            int x=p.poll();
+            sum+=x;
+            p.offer(x/2);
+        }
+        return sum;
+    }
+};
